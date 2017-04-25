51dced5dcff78ac48d05f09a389f6499379cf5f9
Li <lshi7@ncsu.edu>
2017-04-25 00:03:05 -0400

    add random loss

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ import sys
 
 # Get input
 # host, port, send_file, N, MSS = sys.argv[1], int(sys.argv[2]), sys.argv[3], int(sys.argv[4]), int(sys.argv[5])
-host, port, send_file, N, MSS = 'Leon-PC', 7735, 'in02.pdf', 10, 10000
+host, port, send_file, N, MSS = socket.gethostname(), 7735, 'in02.zip', 10, 1000
 
 # Create socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -15,9 +15,9 @@ ack_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ack_socket.bind((host, 62223))
 
 packets = []  # all the packets
-sent_buffer = []  # the packets have been sent
 new_buffer = []  # after receiving ACK, put new packets into new_buffer and send
 
+lock = threading.Lock()
 ack = 0
 
 
@@ -26,68 +26,74 @@ def calc_checksum(data):
     return 0
 
 
+# Send packets
+def socket_send(data):
+    global most_recent_send
+    global lock
+
+    lock.acquire()
+
+    while data:
+        p = data.pop(0)
+        client_socket.sendto(pickle.dumps(p), (host, port))
+        print("Send packet: ", int('0b' + p[0], 2))
+        # update
+        most_recent_send = max(most_recent_send, int('0b' + p[0], 2))
+
+    lock.release()
+
+
 # Listen ACK thread
-def listen_ack(s, h, p):
+def listen_ack(s, h):
     global new_buffer
-    global sent_buffer
     global ack
+    global most_recent_send
+    global most_recent_prepared
 
     # Listen to ACK
     while True:
-        ack_packet, addr = s.recvfrom(256)
+        ack_packet, addr = s.recvfrom(1024)
         ack_packet = pickle.loads(ack_packet)
         ack = int('0b' + ack_packet[0], 2)  # get ack number(next expected packet)
 
         print("Receive ACK: ", ack)
 
-        # print(sent_buffer)
-        # print(new_buffer)
-
-        # Do not consider a "previous" ack
-        if len(packets) > ack > int('0b' + sent_buffer[0][0], 2):
-            # the next packet of sent_buffer[-1](latest packet sent) is the smallest packet we should send
+        if len(packets) > ack:
+            # the next packet of "most recent send packet" or "most recent planed to send packet"
+            # is the smallest packet we should send
             # "ack+N-1" is the largest packet number we should send
-            for j in range(int('0b' + sent_buffer[-1][0], 2)+1, min(len(packets), ack+N)):
+            for j in range(max(most_recent_send+1, most_recent_prepared+1), min(len(packets), ack+N)):
                 # In this thread, only send new packet (slide the window to next)
-                if packets[j] not in sent_buffer:
-                    new_buffer.append(packets[j])
-                    print("prepare: ", j)
+                most_recent_prepared = max(most_recent_prepared, j)
+                new_buffer.append(packets[j])
+                print("prepare: ", j)
         elif ack == len(packets):
             print("Success!!!")
             break
 
 
-# Send packets
+# Send thread
 def send_packet(h, p):
     global new_buffer
-    global sent_buffer
     global cur_time
 
     while True:
         # send packet from new_buffer
         while new_buffer:
-            packet = new_buffer.pop(0)
-            # update right side of sent_buffer (sliding window)
-            sent_buffer.append(packet)
-            client_socket.sendto(pickle.dumps(packet), (host, port))
-            print("Send packet: ", int('0b' + packet[0], 2))
-            # update left side of sent_buffer (sliding window)
-            if len(sent_buffer) > N:
-                sent_buffer.pop(0)
-
+            socket_send(new_buffer)
             cur_time = time.time()
-
-            # t = threading.Timer(10, timer).start()
 
 
 def timer():
     global cur_time
+    resend_buffer = []
     while True:
-        if time.time() >= cur_time + 1:
+        if time.time() >= cur_time + 0.1:
             # Prepare resend packets
             for k in range(ack, min(len(packets), ack+N)):
-                client_socket.sendto(pickle.dumps(packets[k]), (host, port))
-                print("ReSend packet: ", int('0b' + packets[k][0], 2))
+                resend_buffer.append(packets[k])
+            print("Timeout, sequence number = ", ack)
+            socket_send(resend_buffer)
             cur_time = time.time()
         if ack == len(packets):
             print("Success!!!")
@@ -108,15 +114,17 @@ with open(send_file, 'rb') as f:
         else:
             break
 
+
 # Build packets to be sent at window size in the very beginning, then listen to ACK and send packets
 # (Hence this part only execute once)
 for i in range(min(N, len(packets))):
-    # client_socket.sendto(pickle.dumps(packets[i]), (host, port))
     new_buffer.append(packets[i])
 
 cur_time = time.time()
 
+most_recent_send = 0
+most_recent_prepared = min(N-1, len(packets)-1)
 
 threading.Thread(target=send_packet, args=(host, port)).start()
-threading.Thread(target=listen_ack, args=(ack_socket, host, 62223)).start()
+threading.Thread(target=listen_ack, args=(ack_socket, host)).start()
 threading.Thread(target=timer).start()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import pickle
 import threading
+import random
 import sys
 
 # packet = [seq(32bit), checksum(16bit), packet_type(16bit), data]
@@ -10,7 +11,7 @@ next_seq = 0
 
 # get input
 # server_port, out_file, p = int(sys.argv[1]), sys.argv[2], float(sys.argv[3])
-server_port, out_file, p = 7735, 'out03.pdf', 0.1
+server_port, out_file, prob = 7735, 'out03.zip', 0.05
 
 # set server
 host = socket.gethostname()
@@ -41,22 +42,28 @@ def listen(s, h, p):
         # unserialize reveived List
         packet = pickle.loads(recvd_data)
         seq, checksum, packet_type, data = packet[0], packet[1], packet[2], packet[3]
-        print("Receive: ", int('0b'+seq, 2))
+        # print("Receive: ", int('0b'+seq, 2))
 
-        re_checksum = calc_checksum()
-
-        if int('0b'+seq, 2) == next_seq:  # and re_checksum == checksum:
-            next_seq += 1
-
-            # prepare ACK
-            acks.append([bin(next_seq)[2:].zfill(32), bin(0)[2:].zfill(16), '1010101010101010'])
-
-            # write file
-            with open(out_file, 'ab') as f:
-                f.write(data)
+        # Random lose packet
+        if random.random() < prob:
+            print("Packet loss, sequence number = ", int('0b'+seq, 2))
 
         else:
-            continue
+            re_checksum = calc_checksum()
+
+            if int('0b'+seq, 2) == next_seq:  # and re_checksum == checksum:
+                print("Receive: ", int('0b' + seq, 2))
+                next_seq += 1
+
+                # prepare ACK
+                acks.append([bin(next_seq)[2:].zfill(32), bin(0)[2:].zfill(16), '1010101010101010'])
+
+                # write file
+                with open(out_file, 'ab') as f:
+                    f.write(data)
+
+            else:
+                continue
 
 threading.Thread(target=listen, args=(server_socket, host, server_port)).start()
 threading.Thread(target=send_ack, args=(host, 62223)).start()
